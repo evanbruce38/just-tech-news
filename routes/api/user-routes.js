09d157d5f03b7affdb7a39239a4c902b2ce27d02
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-09-05 21:57:09 -0400

    finished password functionality

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,34 @@ router.post('/', (req, res) => {
     });
 });
 
+// login function
+router.post('/login', (req, res) => {
+    // expects...
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email adress!' });
+            return;
+        }
+
+        // res.json({ user: dbUserData });
+
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password! '});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in! '});
+
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
