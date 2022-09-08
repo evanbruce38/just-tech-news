f62bd8d736ed6a614a0f847a6c3792fecb128cb7
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-09-08 17:29:25 -0400

    created homepage

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
